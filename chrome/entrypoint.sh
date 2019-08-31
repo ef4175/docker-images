b1afe20fab995ad49f7d0b37b7d6695a3660dbe8
Edward Fong <ef4175@gmail.com>
2019-08-31 11:07:46 -0400

    Add pulseaudio to chrome image

---
+++ chrome/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+groupadd -f -g $USER_GID $USER_NAME
+adduser --disabled-login --uid $USER_UID --gid $USER_GID --gecos '' $USER_NAME
+sudo -HEu $USER_NAME google-chrome $@
--- chrome/run.sh
+++ chrome/run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t chrome .
-xhost +local:root
-docker run -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --rm chrome google-chrome --no-sandbox
+docker run -it -e DISPLAY=$DISPLAY -e USER_UID=`id -u` -e USER_GID=`id -g` -e USER_NAME=foobar -e PULSE_SERVER=/run/pulse/native -v /tmp/.X11-unix:/tmp/.X11-unix -v /run/user/1000/pulse:/run/pulse --cap-add=SYS_ADMIN --rm chrome $@
