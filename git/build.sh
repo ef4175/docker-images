36ae95d0d407b4e942b437a91c447e9ecc6f90ad
Edward Fong <ef4175@gmail.com>
2020-11-06 23:45:51 +0000

    Enable docker buildkit

--- git/build.sh
+++ git/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --build-arg USER_UID=`id -u` --build-arg USER_GID=`id -g` --build-arg USER_NAME=`whoami` -t git .
+DOCKER_BUILDKIT=1 docker build --build-arg USER_UID=`id -u` --build-arg USER_GID=`id -g` --build-arg USER_NAME=`whoami` -t git .
 chmod 755 git
