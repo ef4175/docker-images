94a538974a445247d109f073a7a5c045437f704a
Edward Fong <ef4175@gmail.com>
2020-11-07 21:20:12 +0000

    Mount directories for data persistence

--- firefox-nightly/run.sh
+++ firefox-nightly/run.sh
@@ -2,4 +2,7 @@
 
 USER_NAME=`whoami`
 USER_UID=`id -u`
-docker run -it -e DISPLAY=$DISPLAY -e PULSE_SERVER=/run/pulse/native -v /tmp/.X11-unix:/tmp/.X11-unix -v /run/user/$USER_UID/pulse:/run/pulse --rm firefox-nightly /home/$USER_NAME/firefox/firefox
+mkdir -p cache
+mkdir -p mozilla
+mkdir -p downloads
+docker run -it -e DISPLAY=$DISPLAY -e PULSE_SERVER=/run/pulse/native -v /tmp/.X11-unix:/tmp/.X11-unix -v /run/user/$USER_UID/pulse:/run/pulse -v `pwd`/cache:/home/$USER_NAME/.cache -v `pwd`/mozilla:/home/$USER_NAME/.mozilla -v `pwd`/downloads:/home/$USER_NAME/Downloads --rm firefox-nightly /home/$USER_NAME/firefox/firefox
