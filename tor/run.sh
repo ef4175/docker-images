eaf99d6605c4642b4ee11293d45d1257dd8051c9
Edward Fong <ef4175@gmail.com>
2020-12-28 23:11:21 +0000

    Create Tor image

---
+++ tor/run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+USER_NAME=`whoami`
+USER_UID=`id -u`
+if [ ! -d TorBrowser ]; then
+  echo 'First run'
+  CONTAINER_ID=`docker run -d tor bash`
+  docker cp \
+    $CONTAINER_ID:/home/$USER_NAME/tor-browser_en-US/Browser/TorBrowser \
+    TorBrowser
+  docker rm -f $CONTAINER_ID
+fi
+mkdir -p downloads
+docker run \
+  -it \
+  -e DISPLAY=$DISPLAY \
+  -e PULSE_SERVER=/run/pulse/native \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v /run/user/$USER_UID/pulse:/run/pulse \
+  -v `pwd`/downloads:/home/$USER_NAME/tor-browser_en-US/Browser/Downloads \
+  -v `pwd`/TorBrowser:/home/$USER_NAME/tor-browser_en-US/Browser/TorBrowser \
+  --shm-size=2g \
+  --rm \
+  tor /home/$USER_NAME/tor-browser_en-US/Browser/start-tor-browser
