919cb5b2d51d3fa38ec47953f5386253adffcca0
Edward Fong <ef4175@gmail.com>
2021-01-17 17:39:43 +0000

    Add API URL to check proxying

--- tor-socks5/run.sh
+++ tor-socks5/run.sh
@@ -2,3 +2,4 @@
 
 docker run -it -p 9050:9050 --rm tor-socks5 tor
 # Allows `curl --socks5-hostname 127.0.0.1:9050` on host
+# Try `curl --socks5-hostname 127.0.0.1:9050 https://check.torproject.org/api/ip`
