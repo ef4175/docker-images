bbfbdefc30c7e63711fed02f74b63769bdbbcdb8
Edward Fong <ef4175@gmail.com>
2021-03-08 22:12:35 +0000

    Chrome is spyware

--- chrome/run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-DOCKER_BUILDKIT=1 docker build -t chrome .
-docker run -it -e DISPLAY=$DISPLAY -e USER_UID=`id -u` -e USER_GID=`id -g` -e USER_NAME=foobar -e PULSE_SERVER=/run/pulse/native -v /tmp/.X11-unix:/tmp/.X11-unix -v /run/user/1000/pulse:/run/pulse --cap-add=SYS_ADMIN --rm chrome $@
