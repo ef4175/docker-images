e01b4438db1daaf319f5c6f1780c716fc03e3d5a
Edward Fong <ef4175@gmail.com>
2021-07-04 19:49:54 +0000

    Prettify bash scripts

--- firefox/run.sh
+++ firefox/run.sh
@@ -5,4 +5,14 @@ USER_UID=`id -u`
 mkdir -p cache
 mkdir -p mozilla
 mkdir -p downloads
-docker run -it -e DISPLAY=$DISPLAY -e PULSE_SERVER=/run/pulse/native -v /tmp/.X11-unix:/tmp/.X11-unix -v /run/user/$USER_UID/pulse:/run/pulse -v `pwd`/cache:/home/$USER_NAME/.cache -v `pwd`/mozilla:/home/$USER_NAME/.mozilla -v `pwd`/downloads:/home/$USER_NAME/Downloads --shm-size=2g --rm firefox /home/$USER_NAME/firefox/firefox
+docker run -it \
+  -e DISPLAY=$DISPLAY \
+  -e PULSE_SERVER=/run/pulse/native \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v /run/user/$USER_UID/pulse:/run/pulse \
+  -v `pwd`/cache:/home/$USER_NAME/.cache \
+  -v `pwd`/mozilla:/home/$USER_NAME/.mozilla \
+  -v `pwd`/downloads:/home/$USER_NAME/Downloads \
+  --shm-size=2g \
+  --rm \
+  firefox /home/$USER_NAME/firefox/firefox
--- git/build.sh
+++ git/build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-DOCKER_BUILDKIT=1 docker build --build-arg USER_UID=`id -u` --build-arg USER_GID=`id -g` --build-arg USER_NAME=`whoami` -t git .
+DOCKER_BUILDKIT=1 docker build \
+  --build-arg USER_UID=`id -u` \
+  --build-arg USER_GID=`id -g` \
+  --build-arg USER_NAME=`whoami` \
+  -t git .
 chmod 755 git
