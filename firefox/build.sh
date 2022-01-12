9bdf21b1432d56209a33b048d7ef48299162611c
Edward Fong <ef4175@gmail.com>
2022-01-12 01:49:45 +0000

    buildx

--- firefox/build.sh
+++ firefox/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DOCKER_BUILDKIT=1 docker build \
+docker buildx build \
   --build-arg USER_UID=`id -u` \
   --build-arg USER_GID=`id -g` \
   --build-arg USER_NAME=`whoami` \
